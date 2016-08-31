3d24d8bca9936157950ae8f87e3f42a4770fd935
Igor Savchuk <trollfred@gmail.com>
2016-08-31 03:40:34 +0300

    fix identation

--- fix_product.sql
+++ fix_product.sql
@@ -2,6 +2,7 @@ UPDATE product
 LEFT JOIN (
 	SELECT product_id, participant_id FROM (
 		SELECT * FROM assignment ORDER BY ts DESC, action DESC
-	) AS sorted GROUP BY product_id
+	) AS sorted
+	GROUP BY product_id
 ) AS latest
 ON id = product_id SET current_owner_id = participant_id;
